49ef7d82f00c44cb990d3d36f59eda98f5017fbc
Shady Mohamed <96765781+ShadiM0hamed@users.noreply.github.com>
2023-10-18 22:41:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def main():
         m2 = translator.translate(m) # Text to be translated
         print(m2)
         st.markdown(f"**Translated ID Text:** {m}")
-        st.markdown("**Translated Name Text:** {}".format(response_Name.text_annotations[0].description.split('\n'))
+        st.markdown("**Translated Name Text:** {}".format(response_Name.text_annotations[0].description.split('\n')))
 
 if __name__ == "__main__":
     main()
