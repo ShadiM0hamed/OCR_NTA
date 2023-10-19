4ae75f77b153e01932fbb654f53fd18335ab5a5f
Shady Mohamed <96765781+ShadiM0hamed@users.noreply.github.com>
2023-10-19 19:24:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -37,24 +37,25 @@ def process_image(image_path):
     if response.status_code == 200:
         output_image = Image.open(BytesIO(response.content))
         output_image = np.array(output_image)
+
+        gray = cv2.cvtColor(output_image, cv2.COLOR_RGBA2GRAY)
+        edged = cv2.Canny(gray, 30, 200)
+
+        contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        roi_images = []
+
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
+            roi = output_image[y:y+h, x:x+w]
+            roi_images.append(roi)
+
+        for i, roi in enumerate(roi_images):
+            break
+
+        return roi
     else:
         print(f"Error: {response.content}")
-
-    gray = cv2.cvtColor(output_image, cv2.COLOR_RGBA2GRAY)
-    edged = cv2.Canny(gray, 30, 200)
-
-    contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    roi_images = []
-
-    for contour in contours:
-        x, y, w, h = cv2.boundingRect(contour)
-        roi = output_image[y:y+h, x:x+w]
-        roi_images.append(roi)
-
-    for i, roi in enumerate(roi_images):
-        break
-
-    return roi
+        return None
 
 
 def main():
