87b2cd965e78721c1f9d3db96179d37d849cb2ea
Shady Mohamed <96765781+ShadiM0hamed@users.noreply.github.com>
2023-10-19 19:35:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,58 +11,96 @@ import streamlit as st
 
 
 def remove_non_english_arabic(text):
+    # Define the regex pattern for English and Arabic characters
+
+    # Join the matches back into a string
     cleaned_text = ' '
     cleaned_text = cleaned_text.replace(" ", "")
     if cleaned_text == '.':
-        cleaned_text = '0'
+      cleaned_text = '0'
     elif cleaned_text == '؛':
-        cleaned_text = '4'
+      cleaned_text = '4'
     elif cleaned_text == '،' or cleaned_text == ',':
-        cleaned_text = '0'
+      cleaned_text = '0'
     elif cleaned_text == ' ':
-        pass
+      pass
     cleaned_text = text
     return cleaned_text
 
 
-def process_image(image_path):
-    api_key = "ku2unwTtxXA4CcBYaMPibcQM"
-    endpoint = "https://api.remove.bg/v1.0/removebg"
-    headers = {"X-Api-Key": api_key}
 
+# Set your API key
+api_key = "bD3oqNdx8FZyPeuv7UAuUZgV"
+
+# Define the API endpoint
+endpoint = "https://api.remove.bg/v1.0/removebg"
+
+# Prepare the headers
+headers = {
+    "X-Api-Key": api_key,
+}
+
+
+
+def process_image(image_path):
+    
+
+    # Load the image
     image = cv2.imread(image_path)
-    files = {"image_file": open(image_path, "rb")}
+
+        # Set your API key
+    files = {  "image_file": open(image_path, "rb"),}
+
+    # Call the API
     response = requests.post(endpoint, headers=headers, files=files)
 
+    # Check if the response is successful
     if response.status_code == 200:
+        # Read the output image
         output_image = Image.open(BytesIO(response.content))
         output_image = np.array(output_image)
 
-        gray = cv2.cvtColor(output_image, cv2.COLOR_RGBA2GRAY)
-        edged = cv2.Canny(gray, 30, 200)
-
-        contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        roi_images = []
-
-        for contour in contours:
-            x, y, w, h = cv2.boundingRect(contour)
-            roi = output_image[y:y+h, x:x+w]
-            roi_images.append(roi)
-
-        for i, roi in enumerate(roi_images):
-            break
-
-        return roi
+        # Show the image using cv2_imshow
+        cv2_imshow(output_image)
     else:
         print(f"Error: {response.content}")
-        return None
+
+    gray = cv2.cvtColor(output_image, cv2.COLOR_RGBA2GRAY) 
+    edged = cv2.Canny(gray, 30, 200) 
+
+    # Finding Contours 
+    contours, hierarchy = cv2.findContours(edged, 
+      cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
+
+    # Print the number of contours found
+    #print("Number of Contours found = " + str(len(contours))) 
+
+    # List to store ROI images
+    roi_images = []
+
+    # Draw bounding rectangles and extract ROI
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        roi = output_image[y:y+h, x:x+w]  # Extract ROI
+        roi_images.append(roi)
+
+    # Show extracted ROIs
+    for i, roi in enumerate(roi_images):
+        #cv2_imshow(roi)
+        #cv2.imwrite(f"/content/roi_f.png", roi)  # Save the ROI
+        break
+    # Return the result
+    return roi
+
+
 
 
 def main():
     st.title("Streamlit App for Image Processing")
 
     uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-    st.image(uploaded_file)
+    print(type(uploaded_file))
+    
     cv2.imwrite('uploaded_image.jpg',uploaded_file)
     if uploaded_file is not None:
         with open('uploaded_image.jpg', 'wb') as f:
